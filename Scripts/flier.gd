1318ef437cd95445672e3a579cf0baedcd87aac3
Justin Graham <justin.m.graham2@gmail.com>
2026-02-08 00:41:09 -0500

    implemented flying enemy

---
+++ Scripts/flier.gd
@@ -0,0 +1,104 @@
+extends CharacterBody2D
+
+@export var patrol_speed: float = 120
+@export var patrol_acc: float = 250
+@export var dive_speed: float = 200
+@export var recovery_speed: float = 160
+@export var alignment_margin: float = 20
+
+enum {
+	PATROL,
+	PLUNGE,
+	RECOVER
+}
+
+var state = PATROL
+var direction = 1
+var start_height = 0.0
+var player: Node2D
+var is_dead: bool = false
+
+@onready var left_bound = $LeftBound.global_position.x
+@onready var right_bound = $RightBound.global_position.x
+@onready var sprite = $Sprite2D
+
+func _ready() -> void:
+	start_height = global_position.y
+
+func _physics_process(delta: float) -> void:
+	match state:
+		PATROL:
+			patrol(delta)
+		PLUNGE:
+			plunge()
+		RECOVER:
+			return_to_patrol(delta)
+
+	move_and_slide()
+
+func patrol(delta: float):
+	var target_speed = patrol_speed * direction
+	velocity.x = move_toward(velocity.x, target_speed, patrol_acc * delta)
+	velocity.y = 0
+	
+	if global_position.x <= left_bound:
+		direction = 1
+	if global_position.x >= right_bound:
+		direction = -1
+		
+	flip_sprite()
+	check_for_player()
+	
+func check_for_player():
+	if not player:
+		return
+		
+	if abs(player.global_position.x - global_position.x) <= alignment_margin:
+		state = PLUNGE
+		velocity = Vector2.ZERO
+	
+func plunge():
+	velocity.x = 0
+	velocity.y = dive_speed
+	
+	if is_on_floor():
+		state = RECOVER
+
+func return_to_patrol(delta: float):
+	velocity.x = 0
+	velocity.y = move_toward(velocity.y, -recovery_speed, recovery_speed * delta)
+	
+	if global_position.y <= start_height:
+		global_position.y = start_height
+		velocity = Vector2.ZERO
+		state = PATROL
+
+func flip_sprite():
+	sprite.flip_h = direction > 0
+
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		player = body
+
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		player = null
+
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if is_dead:
+		return
+
+	if body.is_in_group("Player"):
+		body.take_damage()
+
+	if body.is_in_group("PlayerAttack"):
+		take_damage()
+		
+func take_damage():
+	is_dead = true
+	
+	await get_tree().create_timer(0.4).timeout
+	queue_free()
