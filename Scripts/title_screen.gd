3429376d0c035f1bedcf80cc380967186787e7c3
Justin Graham <justin.m.graham2@gmail.com>
2026-02-19 01:13:17 -0500

    WIP refactor of title screen

--- Scripts/title_screen.gd
+++ Scripts/title_screen.gd
@@ -1,7 +1,25 @@
 extends Control
 
+@onready var save_slots_panel = $SaveSlotsPanel
+
 func _on_start_pressed() -> void:
 	pass # Replace with function body.
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_slot_1_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_slot_2_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_slot_3_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_back_pressed() -> void:
+	pass # Replace with function body.
