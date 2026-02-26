0624911252fcb315aca008ff52ec1fed43431644
J-Graham22 <justin.m.graham2@gmail.com>
2026-02-26 18:30:36 -0500

    added sound effects and more progress on intro cutscene

--- Scripts/intro_cutscene.gd
+++ Scripts/intro_cutscene.gd
@@ -4,8 +4,14 @@ extends Node2D
 
 @onready var jamie_sprite = $Jamie
 @onready var jess_sprite = $Jess
+@onready var dialogue_ui = $DialogueUI
+@onready var dialogue_label = $DialogueUI/Panel/RichTextLabel
 
 @onready var character = GameState.selected_character
+
+var jamie_dialogue = 'Hello Jessie! Wait, you’re not Jessie? Well, while you’re here, could you do me a favor? I’m trying to make ‘Pepere’s Bread’ but I can’t seem to find all the ‘Ingredients’. If you could bring me the ‘Ingredients’ I can make Pepere’s delicious bread. Oh thank you so much! Come back later and I will give you some of ‘Pepere’s Bread’ while it’s still warm.'
+
+var jess_dialogue = 'Hello Jessie! While you’re here, could you do me a favor? I’m trying to make ‘Pepere’s Bread’ but I can’t seem to find all the ‘Ingredients’. If you could bring me the ‘Ingredients’ I can make Pepere’s delicious bread. Oh thank you so much! Come back later and I will give you some of ‘Pepere’s Bread’ while it’s still warm.'
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,8 +21,10 @@ func _ready() -> void:
 
 func run_cutscene():
 	if character == 0:
+		dialogue_label.text = jess_dialogue
 		jess_intro()
 	else:
+		dialogue_label.text = jamie_dialogue
 		jamie_intro()
 
 func jess_intro():
@@ -29,8 +37,14 @@ func jess_intro():
 	animator.play("jess_enter")
 	await animator.animation_finished
 	
+	animator.play("show_dialogue")
+	await animator.animation_finished
+	
 	AudioManager.intro_jess.play()
 	await AudioManager.intro_jess.finished
+	
+	animator.play("hide_dialogue")
+	await animator.animation_finished
 	
 	jess_sprite.flip_h = true
 	animator.play_backwards("jess_enter")
@@ -49,8 +63,14 @@ func jamie_intro():
 	animator.play("jami3_enter")
 	await animator.animation_finished
 	
+	animator.play("show_dialogue")
+	await animator.animation_finished
+	
 	AudioManager.intro_jamie.play()
 	await AudioManager.intro_jamie.finished
+	
+	animator.play("hide_dialogue")
+	await animator.animation_finished
 	
 	jamie_sprite.flip_h = true
 	animator.play_backwards("jami3_enter")
